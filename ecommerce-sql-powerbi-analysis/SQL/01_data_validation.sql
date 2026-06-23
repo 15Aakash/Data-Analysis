9841988aa2c69a6c7917892ab393f71461b5ab9b
Aakash Kathirvel <119592109+15Aakash@users.noreply.github.com>
2026-06-23 19:00:13 -0400

    01_data_validation.sql

--- ecommerce-sql-powerbi-analysis/SQL/01_data_validation.sql
+++ ecommerce-sql-powerbi-analysis/SQL/01_data_validation.sql
@@ -1,1 +1,24 @@
+USE Ecommerce_Analysis;
 
+-- =============================================
+-- 01_data_validation.sql
+-- Purpose:
+-- Check whether all imported tables have correct row counts.
+-- This step helps confirm that the CSV files were imported successfully.
+-- =============================================
+
+SELECT 'customers' AS table_name, COUNT(*) AS total_rows FROM customers
+UNION ALL
+SELECT 'orders', COUNT(*) FROM orders
+UNION ALL
+SELECT 'order_items', COUNT(*) FROM order_items
+UNION ALL
+SELECT 'order_payments', COUNT(*) FROM order_payments
+UNION ALL
+SELECT 'order_reviews', COUNT(*) FROM order_reviews
+UNION ALL
+SELECT 'products', COUNT(*) FROM products
+UNION ALL
+SELECT 'sellers', COUNT(*) FROM sellers
+UNION ALL
+SELECT 'product_translation', COUNT(*) FROM product_translation;
